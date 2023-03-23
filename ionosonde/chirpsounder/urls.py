f66e01cf68eebbc0975d0d319c1b0ca01ba4cdf8
Vladimi <ovladimir848@gmail.com>
2023-03-23 16:48:03 +0300

    CRUD on viewsets

--- ionosonde/chirpsounder/urls.py
+++ ionosonde/chirpsounder/urls.py
@@ -1,21 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (APIConfig, APIConfigDetail,
-                    ScheduleList, ScheduleDetail,
-                    SwtracksViewSet)
+from .views import (ConfigViewSet, ScheduleViewSet, SwtracksViewSet)
 
 
 app_name = 'chirpsounder'
 
 router = DefaultRouter()
 
+router.register('config', ConfigViewSet)
 router.register('swtracks', SwtracksViewSet)
+router.register('schedule', ScheduleViewSet)
 
 urlpatterns = [
-    path('config/', APIConfig.as_view()),
-    path('config/<int:pk>/', APIConfigDetail.as_view()),
-    path('schedule/', ScheduleList.as_view()),
-    path('schedule/<int:pk>/', ScheduleDetail.as_view()),
     path('', include(router.urls)),
 ]
--- ionosonde/chirpsounder/views.py
+++ ionosonde/chirpsounder/views.py
@@ -11,61 +11,9 @@ from .serializers import (ConfigSettingsSerializer, SwtracksSerializer,
                           ScheduleSettingsSerializer)
 
 
-class APIConfig(APIView):
-    def get(self, request):
-        configs = get_list_or_404(ConfigSettings)
-        serializer = ConfigSettingsSerializer(configs, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ConfigSettingsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class APIConfigDetail(APIView):
-    def get(self, request, pk):
-        config = get_object_or_404(ConfigSettings, id=pk)
-        serializer = ConfigSettingsSerializer(config)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk):
-        config = get_object_or_404(ConfigSettings, id=pk)
-        serializer = ConfigSettingsSerializer(config, data=request.data,
-                                              partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk):
-        config = get_object_or_404(ConfigSettings, id=pk)
-        serializer = ConfigSettingsSerializer(config, data=request.data,
-                                              partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        config = get_object_or_404(ConfigSettings, id=pk)
-        config.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def api_swtracks(request):
-    if request.method == 'POST':
-        serializer = SwtracksSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    swtracks_list = get_list_or_404(Swtracks)
-    serializer = SwtracksSerializer(swtracks_list, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+class ConfigViewSet(viewsets.ModelViewSet):
+    queryset = ConfigSettings.objects.all()
+    serializer_class = ConfigSettingsSerializer
 
 
 class SwtracksViewSet(viewsets.ModelViewSet):
@@ -73,28 +21,6 @@ class SwtracksViewSet(viewsets.ModelViewSet):
     serializer_class = SwtracksSerializer
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def api_swtracks_detail(request, pk):
-    swtrack = get_object_or_404(Swtracks, id=pk)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        serializer = SwtracksSerializer(swtrack, data=request.data,
-                                        partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        swtrack.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    serializer = SwtracksSerializer(swtrack)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class ScheduleList(generics.ListCreateAPIView):
-    queryset = get_list_or_404(ScheduleSettings)
-    serializer_class = ScheduleSettingsSerializer
-
-
-class ScheduleDetail(generics.RetrieveUpdateDestroyAPIView):
+class ScheduleViewSet(viewsets.ModelViewSet):
     queryset = ScheduleSettings.objects.all()
     serializer_class = ScheduleSettingsSerializer
